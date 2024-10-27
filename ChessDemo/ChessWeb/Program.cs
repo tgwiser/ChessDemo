70906dd18d4a2fce10eb6ddd00016eae7eb5c4f8
tgwiser <tgwiser@gmail.com>
2024-10-27 11:46:29 +0200

    #Fix web app

--- ChessDemo/ChessWeb/Program.cs
+++ ChessDemo/ChessWeb/Program.cs
@@ -1,4 +1,5 @@
-using ChessWeb.Data;
+using ChessCommon.Evaluators;
+using ChessWeb;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -7,7 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddSingleton<IPositionEvaluator, PositionEvaluator>();
+builder.Services.AddSingleton<ChessService>();
 
 var app = builder.Build();
 
